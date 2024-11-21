af1620d0fc8283f97e8914050c132a81e26e94ca
alanandrewvarghese <alanandrew883@gmail.com>
2024-11-21 23:11:15 +0530

    Update portfolio

---
+++ app/static/js/script.js
@@ -0,0 +1,9 @@
+// Navigation
+
+const navToggle = document.getElementById('nav-toggle');
+const navLinks = document.getElementById('nav-links');
+
+navToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('hidden');
+});
+
